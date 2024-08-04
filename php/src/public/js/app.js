256075baf7c429e1c3e35c1b65ee749f314d81e9
Brett Spaulding <brett.spaulding@stonesoft.io>
2024-08-04 17:08:20 -0400

    [WIP] Front-end moved to PHP

---
+++ php/src/public/js/app.js
@@ -0,0 +1,60 @@
+const appModal = $('#modalDownloadQueue');
+const appModalContent = $('#modal_content');
+let modalPolling = false;
+
+function proc_notification(icon, title, text) {
+    Swal.fire({
+        title: title,
+        icon: icon,
+        text: text
+    })
+}
+
+function fill_download_queue() {
+    $.ajax({
+        url: '/api/v1/get/queue'
+    }).done((res) => {
+        appModalContent.html(res);
+    })
+}
+
+$('.settings_btn').on('click', () => {
+    $('#modalSettings').modal('toggle');
+})
+
+$('.queue_btn').on('click', () => {
+    console.log('Get Queue!');
+    if (modalPolling) {
+        clearInterval(modalPolling);
+    }
+    fill_download_queue();
+    modalPolling = setInterval(fill_download_queue, 12000);
+    appModal.modal('toggle');
+})
+
+$('#download_btn').on('click', () => {
+    let artist = $('#search_bar').val();
+    // Prevent
+    $('#search_bar').val('');
+    let icon = 'error';
+    let title = 'What the flip?!';
+    let text = 'You need to add an artist bro..';
+
+    if (artist) {
+        $("#loader-wrapper").fadeIn(300);
+        $.ajax({
+            url: `/api/v1/get/artist/${artist}`,
+        }).done(function (res) {
+            text = res.message;
+            if (res.status === 200) {
+                icon = 'success';
+                title = 'Shazam!';
+            }
+            $("#loader-wrapper").fadeOut(700);
+            proc_notification(icon, title, text);
+        });
+    } else {
+        proc_notification(icon, title, text);
+    }
+
+})
