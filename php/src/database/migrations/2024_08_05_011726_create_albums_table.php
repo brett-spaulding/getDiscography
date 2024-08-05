6c049a16750ec83d8c2782afa81963e1e4b36981
Brett Spaulding <brett.spaulding@stonesoft.io>
2024-08-04 21:48:52 -0400

    [ADD] Models: Artist, Album and Song

---
+++ php/src/database/migrations/2024_08_05_011726_create_albums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Artist;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->foreignId('artist_id')->constrained(Artist::class);
+            $table->string('url_remote')->nullable();
+            $table->string('url_local')->nullable();
+            $table->enum('state', [
+                'pending',
+                'in_progress',
+                'done',
+            ])->default('pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
